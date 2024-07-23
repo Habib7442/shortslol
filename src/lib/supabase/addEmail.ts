03060e620f857095559e07c49db57dc2ade2e6d0
Habib7442 <habibtanwir1906@gmail.com>
2024-07-23 11:50:10 +0530

    initial commit

--- src/lib/supabase/addEmail.ts
+++ src/lib/supabase/addEmail.ts
@@ -6,22 +6,24 @@ type AddEmailProps = {
   email: string;
 };
 
-// export const addEmail = async ({ email }: AddEmailProps) => {
-//   // Check if the email already exists in the table
-//   const { data, error } = await supabase
-//     .from(EMAILS_TABLE)
-//     .select('email')
-//     .eq('email', email)
-//     .limit(1);
+const EMAILS_TABLE = "emails"
 
-//   if (error) throw error;
+export const addEmail = async ({ email }: AddEmailProps) => {
+  // Check if the email already exists in the table
+  const { data, error } = await supabase
+    .from(EMAILS_TABLE)
+    .select('email')
+    .eq('email', email)
+    .limit(1);
 
-//   // If the email is not found in the table, insert it
-//   if (data && data.length === 0) {
-//     const { error: insertError } = await supabase.from(EMAILS_TABLE).insert({
-//       email,
-//     });
+  if (error) throw error;
 
-//     if (insertError) throw insertError;
-//   }
-// };
+  // If the email is not found in the table, insert it
+  if (data && data.length === 0) {
+    const { error: insertError } = await supabase.from(EMAILS_TABLE).insert({
+      email,
+    });
+
+    if (insertError) throw insertError;
+  }
+};
